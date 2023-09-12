90b3a4c9920bfa38c81478e9b78d0c3959432faa
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2023-09-12 15:13:46 +0330

    Added a couple more apps in there

--- src/lib/List.tsx
+++ src/lib/List.tsx
@@ -12,7 +12,7 @@ const cards = [
     website: "https://www.google.com/chrome/",
     dlURL: "https://www.google.com/chrome/",
     price: "free",
-    platforms: "WMLAI",
+    platforms: "PC & Phones",
   },
   {
     id: id++,
@@ -23,7 +23,7 @@ const cards = [
     website: "https://code.visualstudio.com/",
     dlURL: "https://code.visualstudio.com/#alt-downloads",
     price: "free",
-    platforms: "WML",
+    platforms: "PC",
   },
   {
     id: id++,
@@ -34,18 +34,71 @@ const cards = [
     website: "https://bun.sh/",
     dlURL: "https://bun.sh/",
     price: "free",
-    platforms: "ML(WSL)",
+    platforms: "Mac, Linux, (WSL)",
+  },
+  {
+    id: id++,
+    icon: "./images/node.svg",
+    name: "NodeJS",
+    company: "OpenJS Foundation",
+    category: "JS Runtime",
+    website: "https://nodejs.org/",
+    dlURL: "https://nodejs.org/download",
+    price: "free",
+    platforms: "PC",
+  },
+  {
+    id: id++,
+    icon: "./images/deno.svg",
+    name: "Deno",
+    company: "Deno Land Inc.",
+    category: "JS Runtime",
+    website: "https://deno.com/",
+    dlURL: "https://deno.com/manual/getting_started/installation/",
+    price: "free",
+    platforms: "PC",
+  },
+  {
+    id: id++,
+    icon: "./images/git.svg",
+    name: "Git",
+    company: "Linus Torvalds",
+    category: "Version Control System (VCS)",
+    website: "https://git-scm.com/",
+    dlURL: "https://git-scm.com/downloads",
+    price: "free",
+    platforms: "PC",
+  },
+  {
+    id: id++,
+    icon: "./images/github.svg",
+    name: "GitHub",
+    company: "GitHub Inc.",
+    category: "Source Code Hosting Platform",
+    website: "https://github.com/",
+    dlURL: "https://github.com/",
+    price: "free",
+    platforms: "Web",
+  },
+  {
+    id: id++,
+    icon: "./images/windows11.svg",
+    name: "Windows 11",
+    company: "Microsoft Corp.",
+    category: "PC OS",
+    website: "https://www.microsoft.com/en-us/windows/windows-11",
+    dlURL: "https://www.microsoft.com/en-us/windows/get-windows-11",
+    price: "free",
+    platforms: "Windows",
   },
 ];
 
 export default function List() {
   return <div className="list">
-
     {cards.map((card, index) => (
 
       <Card key={ index } data={ card } /> 
       
     ))}
-
   </div>
 }
